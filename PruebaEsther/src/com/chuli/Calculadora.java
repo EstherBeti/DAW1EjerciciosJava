e87b93877a1307c8ef2ab186d06e68eb527a8057
estherbeti <estherbetijimenez@gmail.com>
2022-09-18 20:45:47 +0200

    anhadimos clase de prueba

---
+++ PruebaEsther/src/com/chuli/Calculadora.java
@@ -0,0 +1,37 @@
+package com.chuli;
+
+public class Calculadora {
+
+	public static void main(String[] args) {
+		/*
+		 * int double char boolean
+		 */
+		int primerNumero, segundoNumero, resultado;
+
+		primerNumero = 0;
+		System.out.println("aqui "+ primerNumero);
+		segundoNumero = 2;
+		System.out.println("aqui "+ segundoNumero);
+
+		resultado = primerNumero + segundoNumero;
+		resultado++;
+
+		System.out.println(resultado);
+		String cAdena = null;
+
+
+		cAdena = "asd";
+		
+		cAdena = "asdasd";
+		
+		
+		cAdena = "asdasd";
+		if (cAdena.equals("algo")) {
+			System.out.println("Correcto");
+		}
+
+	
+
+	}
+
+}
